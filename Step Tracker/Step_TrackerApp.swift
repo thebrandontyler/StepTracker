c0d0551ec5612e2f56beace62753cfdb75340818
Brandon Maynard <brandontyler@me.com>
2024-05-13 21:43:48 -0700

    Created Xcode Project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Brandon Tyler on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
